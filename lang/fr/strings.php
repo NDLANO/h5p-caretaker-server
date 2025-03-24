eae1e92297b778c4d6cecf77695148fe40c37f77
Oliver Tacke <o.tacke@posteo.de>
2025-03-24 11:48:42 +0100

    Add French translation

---
+++ lang/fr/strings.php
@@ -0,0 +1,45 @@
+<?php
+
+$string['site:title'] = 'Implémentation de référence H5P Caretaker';
+
+$string['error:fileTooLarge'] = "Le fichier est plus grand que la taille maximale autorisée de %s KB.";
+$string['error:methodNotAllowed'] = 'Méthode non autorisée';
+$string['error:noFileOrTooLarge'] = 'Il semble qu’aucun fichier n’ait été fourni ou qu’il dépasse la taille limite d’envoi de %s KB.';
+$string["error:unknownError"] = "Quelque chose s’est mal passé, mais je ne sais pas quoi, désolé!";
+$string["error:checkServerLog"] = "Veuillez consulter les journaux du serveur.";
+
+$string['h5pcaretaker'] = 'H5P Caretaker';
+$string['headline'] = 'Prenez soin de votre H5P';
+$string['callToAction'] = 'Vérifiez si votre fichier H5P peut être amélioré';
+$string['callToActionDetails'] = "Découvrez les problèmes d’accessibilité, les informations manquantes et les bonnes pratiques qui peuvent vous aider à améliorer votre contenu H5P.";
+
+$string['languageSelect:a11ySelectLanguage'] = 'Choisir votre langue';
+
+$string['filter:allFilteredOut'] = 'Tous les messages ont été filtrés selon leur contenu.';
+$string['filter:contentFilter'] = 'Filtre du type de contenu';
+$string['filter:showAll'] = 'Tout afficher';
+$string['filter:showSelected'] = 'Différents contenus sélectionnés';
+$string['filter:showNone'] = 'Ne rien afficher';
+$string['filter:filterByContent'] = 'Filtrer sur le contenu:';
+$string['filter:reset'] = 'Réinitialiser';
+
+$string['dropzone:orDragTheFileHere'] = 'ou glissez le fichier ici';
+$string['dropzone:removeFile'] = 'Supprimer le fichier';
+$string['dropzone:uploadProgress'] = 'Avancement de l’envoi';
+$string['dropzone:uploadH5PFile'] = 'Envoyez votre fichier H5P';
+$string['dropzone:fileBeingChecked'] = 'Votre fichier est en cours de vérification';
+$string['dropzone:fileCheckedSuccessfully'] = 'Votre fichier a été vérifié avec succès';
+
+$string['results:totalMessages'] = 'Total des messages';
+$string['results:issues'] = 'problèmes';
+$string['results:results'] = 'résultats';
+$string['results:filterBy'] = 'Filtrer par';
+$string['results:groupBy'] = 'Grouper par';
+$string['results:download'] = 'Télécharger';
+$string['results:showDetails'] = 'Voir les détails';
+$string['results:hideDetails'] = 'Cacher les détails';
+
+$string['expand:expandAllMessages'] = 'Déplier tous les messages';
+$string['expand:collapseAllMessages'] = 'Replier tous les messages';
+
+$string['report:TitleTemplate'] = 'Rapport H5P-Caretaker pour @title';
